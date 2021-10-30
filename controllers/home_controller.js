d5c727fb0477f76dee035bc525a01b5cba229b93
Bhagavan <bhagavanmarpadaga@gmail.com>
2021-10-30 22:37:27 +0530

    set up comments and posts parts done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -37,6 +37,7 @@ module.exports.home=function(req,res)
 
     // })
 
+    console.log("IN side home",req.user.id);
     Post.find({}).populate('user').populate({path:'comments',populate:{path:'user'}}).exec(function(err,posts){
 
         
